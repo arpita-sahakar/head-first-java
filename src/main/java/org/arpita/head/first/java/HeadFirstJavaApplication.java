771aea3b5959f552b588119580d5c50201db12fd
Arpita Saha <arpita.sahaa@gmail.com>
2025-10-30 10:52:13 -0400

    initial commit

---
+++ src/main/java/org/arpita/head/first/java/HeadFirstJavaApplication.java
@@ -0,0 +1,13 @@
+package org.arpita.head.first.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadFirstJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/arpita/head/first/java/HeadFirstJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arpita.head.first.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
